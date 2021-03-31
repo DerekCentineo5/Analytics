f6c51fc042f7cf012716c7f31cd62e5ab837a936
Derek Centineo <derekcentineo@gmail.com>
2021-03-31 17:17:52 -0400

    Updated Image path

--- Analysis.py
+++ Analysis.py
@@ -88,7 +88,7 @@ Risk Ranges Tool
 """)
 # Sidebar Header
 
-Falcon = Image.open("/Users/derekcentineo/Analytics/Risk_Ranges_Beta/Falcon.jpeg")
+Falcon = Image.open("/Users/derekcentineo/Documents/GitHub/Analytics/Falcon.jpeg")
 st.sidebar.image(Falcon, use_column_width=False)
 st.sidebar.header('User Input')
 
