dc8f11aeafad76f46a6af4aef1bae78f0d8f836f
Derek Centineo <derekcentineo@gmail.com>
2021-04-12 17:53:24 -0400

    -

--- Falcon_Management_Alt_Data_Dashboard.py
+++ Falcon_Management_Alt_Data_Dashboard.py
@@ -1,17 +1,17 @@
-#import Momentum
+import Momentum
 import Jim_Dash
 import Falcon_Management_Ranges
 import numpy.core.multiarray
 import streamlit as st
 from PIL import Image
 
-#Falcon = Image.open("Falcon.jpeg")
-#st.image(Falcon, use_column_width=False)
+Falcon = Image.open("Falcon.jpeg")
+st.image(Falcon, use_column_width=False)
 
 PAGES = {
     "Daily Ranges": Jim_Dash,
-    "Risk Ranges Analysis": Falcon_Management_Ranges
-    #"Momentum": Momentum
+    "Risk Ranges Analysis": Falcon_Management_Ranges,
+    "Momentum": Momentum
 }
 
 Falcon = Image.open("Falcon.jpeg")
