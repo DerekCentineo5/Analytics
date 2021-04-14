19d675b62333b480f987b801aa53bff048d2aa80
Derek Centineo <derekcentineo@gmail.com>
2021-04-14 08:47:48 -0400

    -

--- Falcon_Management_Alt_Data_Dashboard.py
+++ Falcon_Management_Alt_Data_Dashboard.py
@@ -1,13 +1,12 @@
 import os
 import sys
-import Mom
 import Jim_Dash
 import Falcon_Management_Ranges
 import streamlit as st
 from PIL import Image
 
 PAGES = {
-    "Momentum": Mom,
+    #"Momentum": Mom,
     "Daily Ranges": Jim_Dash,
     "Risk Ranges Analysis": Falcon_Management_Ranges
 }
