4f1f1ae2d5d861d10706848e71d6d5d93a745f91
Derek Centineo <derekcentineo@gmail.com>
2021-04-14 15:57:14 -0400

    -

--- Falcon_Management_Alt_Data_Dashboard.py
+++ Falcon_Management_Alt_Data_Dashboard.py
@@ -2,7 +2,7 @@ import os
 import sys
 import Jim_Dash
 import psycopg2
-import Reddit_Screener
+#import Reddit_Screener
 import Falcon_Management_Ranges
 import streamlit as st
 from PIL import Image
@@ -10,8 +10,8 @@ from PIL import Image
 PAGES = {
     #"Momentum": Mom,
     "Daily Ranges": Jim_Dash,
-    "Risk Ranges Analysis": Falcon_Management_Ranges,
-    "Social Media Sentiment": Reddit_Screener
+    "Risk Ranges Analysis": Falcon_Management_Ranges
+    #"Social Media Sentiment": Reddit_Screener
 }
 
 Falcon = Image.open("Falcon.jpeg")
