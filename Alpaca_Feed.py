f59687f9567d0e12384bf1cd1b9655e2020a8f5f
Derek Centineo <derekcentineo@gmail.com>
2021-04-20 10:22:18 -0400

    -

--- Alpaca_Feed.py
+++ Alpaca_Feed.py
@@ -30,7 +30,7 @@ def app():
     Total_Positions['Qty'] = df3
     Total_Positions['Unrealized P/L %'] = df4*100
     Total_Positions = Total_Positions.rename(columns={0:'Symbol'}).set_index('Symbol')
-    Total_Positions = Total_Positions.style.format({'Unrealized P/L %':"{:.2%}"})
+    Total_Positions = Total_Positions.to_string(formatters={'Unrealized P/L %':"{:.2%}".format})
     Total_Positions
 
     ##### Orders ######
