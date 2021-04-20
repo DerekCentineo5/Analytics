cad9c3cf43bd70a7563f4ba9b5cff3e6177dd0f7
Derek Centineo <derekcentineo@gmail.com>
2021-04-20 15:53:00 -0400

    -

--- Reddit_Dashboard.py
+++ Reddit_Dashboard.py
@@ -7,7 +7,7 @@ import plotly.graph_objects as go
 
 def app():
 
-    option = st.sidebar.selectbox("Which Dashboard?", ('twitter', 'wallstreetbets', 'StockTwits', 'chart', 'pattern'), 4)
+    option = st.sidebar.selectbox("Which Dashboard?", ('StockTwits','twitter', 'wallstreetbets', 'StockTwits', 'chart', 'pattern'), 0)
 
     st.header(option)
 
